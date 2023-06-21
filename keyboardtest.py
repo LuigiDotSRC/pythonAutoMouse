e304d1db53931b80c22c90db49312221d82330de
AndreDotSRC <luigi.terashima@gmail.com>
2023-06-21 13:33:25 -0400

    add new test files

---
+++ keyboardtest.py
@@ -0,0 +1,4 @@
+import pyautogui as p
+import time as t
+t.sleep(5)
+p.typewrite("wasdwasdwasdwasdwasdwasdwasdwasdwasdwasd")
---
+++ scroll.py
@@ -0,0 +1,15 @@
+# test script
+
+import pyautogui as p
+
+for i in range(100):
+    p.move(1,0)
+
+for i in range(100):
+    p.move(0,1)
+
+for i in range(100):
+    p.move(-1,0)
+
+for i in range(100):
+    p.move(0,-1)
